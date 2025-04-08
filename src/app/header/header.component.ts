6f20162094ca832aaa9f8277aa57a450b05b3ac8
jeorge oliver <jeorgeadonaoliver@gmail.com>
2025-04-08 12:54:48 +0800

    Initial commit

---
+++ src/app/header/header.component.ts
@@ -0,0 +1,14 @@
+import { Component } from "@angular/core";
+
+@Component({
+    standalone:true,
+    selector: 'app-header',
+    styleUrl: './header.component.css',
+    templateUrl: './header.component.html'
+})
+
+
+export class HeaderComponent{
+
+
+}
---
+++ src/app/user/user.component.ts
@@ -0,0 +1,32 @@
+import { Component, computed, Input, input} from '@angular/core';
+
+@Component({
+  selector: 'app-user',
+  standalone: true,
+  imports: [],
+  templateUrl: './user.component.html',
+  styleUrl: './user.component.css'
+})
+
+export class UserComponent {
+
+  @Input({required: true}) avatar!: string;
+  @Input({required: true}) name!: string;
+
+  get imagePath(){
+    return 'assets/users/' + this.avatar;
+  }
+
+  // avatar = input.required<string>();
+  // name = input.required<string>();
+
+  // imagePath = computed(() => {
+  //   return 'assets/users/' + this.avatar();
+  // });
+
+
+  onSelectedUser(){
+    console.log('clicked');
+  }
+
+}
